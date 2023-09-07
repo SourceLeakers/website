5c11eb585f96cd6ccee6bb60d2dd6a2df440846a
SourceLeakers <140665443+SourceLeakers@users.noreply.github.com>
2023-09-07 12:39:03 +0100

    Update ipban.js

--- ipban.js
+++ ipban.js
@@ -1,13 +1,13 @@
 const ipToVideo = {
-      "172.56.73.70": "https://www.youtube.com/watch?v=nEPHyO-LqgM",
-      "IP_UNAUTHROIZED": "https://www.youtube.com/watch?v=nEPHyO-LqgM",
+    "172.56.73.70": "https://www.youtube.com/watch?v=VIDEO_ID_1",
+    "IP_ADDRESS_2": "https://www.youtube.com/watch?v=VIDEO_ID_2"
 };
 
-const clientIP = "172.56.73.70"; // Replace with actual IP detection logic if necessary
+const clientIP = "127.0.0.1:5000"; // Replace with actual IP detection logic
 
 if (ipToVideo.hasOwnProperty(clientIP)) {
     const youtubeURL = ipToVideo[clientIP];
     window.location.href = youtubeURL;
 } else {
-   alert("unauthorized to access the page.");
+   alert("unauthorized.");
 }
