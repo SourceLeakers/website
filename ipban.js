59a21ae4947386c3208d5b98487903f25756f4dd
SourceLeakers <140665443+SourceLeakers@users.noreply.github.com>
2023-09-07 12:23:12 +0100

    Update ipban.js

--- ipban.js
+++ ipban.js
@@ -1,6 +1,6 @@
 const ipToVideo = {
       "172.56.73.70": "https://www.youtube.com/watch?v=nEPHyO-LqgM",
-      "154.255.194.186": "https://www.youtube.com/watch?v=nEPHyO-LqgM",
+      "IP_UNAUTHROIZED": "https://www.youtube.com/watch?v=nEPHyO-LqgM",
 };
 
 const clientIP = "172.56.73.70"; // Replace with actual IP detection logic if necessary
